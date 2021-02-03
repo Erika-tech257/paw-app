4ac6187cee840a54d9dd660d4140a2f0e048c8d3
ERika-tech257 <ErikaJtech@gmail.com>
2021-02-02 22:41:40 -0500

    Cloudinary imge upload working and displaying to screen

--- src/components/PawPost/ImagePost.tsx
+++ src/components/PawPost/ImagePost.tsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import APIURL from '../../environment'
+import './ImagePost.css'
 
 interface ImgProps {
     sessionToken: any
 }
 
 type ImgState = {
-    avUrl: string,
-    
-    
+    avUrl: string;
+    loading: boolean;
+
+
 }
 
 const CLOUD_URL = "https://api.cloudinary.com/v1_1/dc7cdwbh0/image/upload"
@@ -18,80 +20,94 @@ class ImagePost extends Component<ImgProps, ImgState> {
         super(props)
 
         this.state = {
-            avUrl: "https://res.cloudinary.com/dc7cdwbh0/image/upload/v1612222371/BallrApp/xsfamylxreyfrijj5kav.jpg" ,
-        //    copy secure url image from console goes into avUrl as default image
-            
+            avUrl: "",
+            loading: false
+            //    copy secure url image from console goes into avUrl as default image
+
         }
         // uploaded image manually to cloudinary
-        this.setState({ avUrl: ("https://res.cloudinary.com/dc7cdwbh0/image/upload/v1612222371/BallrApp/xsfamylxreyfrijj5kav.jpg") })
-    
+        this.setState({ avUrl: "" })
+        // this.setState({ loading: false })
+
     }
-            handleSubmit = async (e: any) => {
-            e.preventDefault()
+    handleSubmit = async (e: any) => {
+        e.preventDefault()
 
-            // 'http://localhost:5000/user/cloudsign'
+        // 'http://localhost:5000/user/cloudsign'
 
-            const response = await fetch(`${APIURL}/user/cloudsign`, {
-                method: 'GET',
-                headers: {
-                    'Authorization': this.props.sessionToken
-                }
-            })
+        const response = await fetch(`${APIURL}/user/cloudsign`, {
+            method: 'GET',
+            headers: {
+                'Authorization': this.props.sessionToken
+            }
+        })
 
-            const { sig, ts } = await response.json()
+        const { sig, ts } = await response.json()
 
-            console.log(ts);
-            
-            // const fileInput = document.getElementById('file-input')
-            // let file
-            // if(fileInput !== null){
-            //     file = (fileInput as HTMLInputElement)!.files[0]
+        console.log(ts);
 
-            // }
-            // const file = HTMLElement.itemId('file-input').files[0]
-            const file = (document.getElementById('file-input') as HTMLInputElement)!.files![0]
-            // const file = (document.getElementById('file-input'))!.files[0]
-            const formData = new FormData()
-        
-            formData.append('file', file)
-            formData.append('upload_preset', 'uuhz0rq7')
-            formData.append('api_key', '513851381862193')
-            formData.append('signature', sig)
-            formData.append('timestamp', ts)
+        // const fileInput = document.getElementById('file-input')
+        // let file
+        // if(fileInput !== null){
+        //     file = (fileInput as HTMLInputElement)!.files[0]
 
-            const results = await (await fetch(CLOUD_URL, {
-                method: "POST",
-                body: formData
-            })).json()
-        
-            console.log(results)
-        
-            this.setState({avUrl:results.secure_url})
+        // }
+        // const file = HTMLElement.itemId('file-input').files[0]
+        const file = (document.getElementById('file-input') as HTMLInputElement)!.files![0]
+        // const file = (document.getElementById('file-input'))!.files[0]
+        const formData = new FormData()
 
-            // 'http://localhost:5000/user/imageset'
-        
-            const final = await (await fetch(`${APIURL}/user/imageset`, {
-                method: 'PUT',
-                headers:{
+        formData.append('file', file)
+        formData.append('upload_preset', 'uuhz0rq7')
+        formData.append('api_key', '513851381862193')
+        formData.append('signature', sig)
+        formData.append('timestamp', ts)
+        this.setState({ loading: true })
+
+        const results = await (await fetch(CLOUD_URL, {
+            method: "POST",
+            body: formData
+        })).json()
+
+        console.log(results)
+
+        this.setState({ avUrl: results.secure_url })
+        this.setState({ loading: false })
+
+        // 'http://localhost:5000/user/imageset'
+
+        const final = await (await fetch(`${APIURL}/user/imageset`, {
+            method: 'PUT',
+            headers: {
                 'Authorization': this.props.sessionToken,
                 'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ url: results.secure_url})
-            })).json()
-        
-            console.log(final);
-        }
-    
-    
+            },
+            body: JSON.stringify({ url: results.secure_url })
+        })).json()
+
+        console.log(final);
+    }
+
+
     render() {
         return (
             <div>
+                {/* <form encType="multipart/form-data" onSubmit={this.handleSubmit}> */}
 
-                <form encType="multipart/form-data" onSubmit={this.handleSubmit}>
-                    <input id="file-input" type="file" />
-                    <button className='loadButton' >Upload!</button>
-                </form>
-                <img src={this.state.avUrl} alt="PetImage" width = "200" height="200" />
+                    <input id="file-input" type="file" 
+                    placeholder="Upload an image"
+                    onChange={this.handleSubmit}
+                    />
+                     {/* <button className='loadButton' >Upload!
+                    </button> */}
+                    {this.state.loading ? (
+                        <h3>Loading...</h3>
+                    ) : (
+                        <img src={this.state.avUrl} alt="PetImage" width="200" height="200" />
+                    )}
+                   
+                {/* // </form> */}
+               
 
             </div>
         )
