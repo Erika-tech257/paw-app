f4135f1a67e415150e5cd51673f62f7d6f2a6b3f
ERika-tech257 <ErikaJtech@gmail.com>
2021-02-26 18:22:32 -0500

    test

--- src/components/PawPost/ImagePost.tsx
+++ src/components/PawPost/ImagePost.tsx
@@ -2,9 +2,14 @@ import React, { Component } from 'react'
 import APIURL from '../../environment'
 import './ImagePost.css'
 
+/* Go into the endpoint and add a user params similar to postcontroller so image is attached to each user. create an img tag or p tag to help display image maybe with postcatalog or similar to commenthistory component. within the put/update fetch pass Obj and dig into the array and grab the secure url of the image(would be in the console).Also use .map(map method to map over images attached to users) & use a ternary in the return to display what the user has uploaded or default image will display. May try to use onChange for update.
+
+*/
+
+
 interface ImgProps {
     sessionToken: any
-    
+
 }
 
 type ImgState = {
@@ -47,12 +52,6 @@ class ImagePost extends Component<ImgProps, ImgState> {
 
         console.log(ts);
 
-        // const fileInput = document.getElementById('file-input')
-        // let file
-        // if(fileInput !== null){
-        //     file = (fileInput as HTMLInputElement)!.files[0]
-
-        // }
         // const file = HTMLElement.itemId('file-input').files[0]
         const file = (document.getElementById('file-input') as HTMLInputElement)!.files![0]
         // const file = (document.getElementById('file-input'))!.files[0]
@@ -93,22 +92,22 @@ class ImagePost extends Component<ImgProps, ImgState> {
     render() {
         return (
             <div>
-                {/* <form encType="multipart/form-data" onSubmit={this.handleSubmit}> */}
+                <form encType="multipart/form-data" onSubmit={this.handleSubmit}>
 
-                    <input id="file-input" type="file" 
-                    placeholder="Upload an image"
-                    onChange={this.handleSubmit}
+                    <input id="file-input" type="file"
+                        placeholder="Upload an image"
+                        onChange={this.handleSubmit}
                     />
-                     {/* <button className='loadButton' >Upload!
-                    </button> */}
+                    <button className='loadButton' >Upload!
+                    </button>
                     {this.state.loading ? (
                         <h3>Loading...</h3>
                     ) : (
                         <img src={this.state.avUrl} alt="PetImage" width="200" height="200" />
                     )}
-                   
-                {/* // </form> */}
-               
+
+                </form>
+
 
             </div>
         )
